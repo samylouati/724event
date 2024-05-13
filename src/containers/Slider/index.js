eb6f2a76a991b9210a1299ef43f7128ecd034b35
samylouati <148778977+samylouati@users.noreply.github.com>
2024-05-13 16:18:29 +0200

    changement event en focus pour le slide

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -27,20 +27,20 @@ const Slider = () => {
   
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
+      {byDateDesc?.map((focus, idx) => (
         <>
           <div
-            key={event.id}
+            key={focus.id}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={focus.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+                <h3>{focus.title}</h3>
+                <p>{focus.description}</p>
+                <div>{getMonth(new Date(focus.date))}</div>
               </div>
             </div>
           </div>
@@ -48,7 +48,7 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={focus.id}
                   type="radio"
                   name="radio-button"
                   defaultChecked={idx === radioIdx}
