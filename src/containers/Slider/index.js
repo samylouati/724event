573faa3c6faf983ee21ae90e661966d42ff6a394
samylouati <148778977+samylouati@users.noreply.github.com>
2024-05-14 10:49:33 +0200

    correction mentorat

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
@@ -11,52 +11,47 @@ const Slider = () => {
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    // Ajout condition pour appel de la fonction
-    if (byDateDesc) {
-      setTimeout(
-        () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-        5000
-      );
-    }
+    setTimeout(
+      () => setIndex(index < (byDateDesc?.length || 0) - 1 ? index + 1 : 0),
+      5000
+    );
   };
   useEffect(() => {
     nextCard();
-  },
-
-  [index]); // J'ajoute un index comme dependance pour eviter les problemes de rendu
-  
+  });
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((focus, idx) => (
-        <>
+      {byDateDesc?.map((event, idx) => (
+        <React.Fragment key={`${event.title}-Fragment`}>
           <div
-            key={focus.id}
+            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={focus.cover} alt="forum" />
+            <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
-                <h3>{focus.title}</h3>
-                <p>{focus.description}</p>
-                <div>{getMonth(new Date(focus.date))}</div>
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((e, radioIdx) => (
                 <input
-                  key={focus.id}
+                  key={`${e.title}-Radio`}
                   type="radio"
                   name="radio-button"
-                  defaultChecked={idx === radioIdx}
+                  checked={index === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
